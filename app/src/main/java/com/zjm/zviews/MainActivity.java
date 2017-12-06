f6fae85c0516984b95cdfba707755eaeaa1cd6c5
zjmjm404 <zhangjm404@gmail.com>
2017-12-06 15:15:44 +0800

    create project

---
+++ app/src/main/java/com/zjm/zviews/MainActivity.java
@@ -0,0 +1,20 @@
+package com.zjm.zviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ *
+ * When I wrote this, only God and I understood what I was doing
+ * Now, God only knows
+ *
+ * @author ${USER}$
+ * @date 2017/12/6
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
